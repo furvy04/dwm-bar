d497305dd31cb5116b004e77d6fca585756399c1
furvy <62027710+furvy04@users.noreply.github.com>
2020-10-12 17:12:13 +0200

    Update dwm-bar.sh

--- dwm-bar.sh
+++ dwm-bar.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#dwm-bar function to display cpu used percent, ram used e date.
+
+#dwm-bar function to display cpu percentage, used ram, date and time.
 #GNU GPL-3.0
 
 while :
 	do
 		
-		# CPU Used Percent
+		# cpu Used Percent
 		CPU=$(awk '{u=$2+$4; t=$2+$4+$5;if (NR==1){u1=u; t1=t;} else printf("%d%%", ($2+$4-u1) * 100 / (t-t1) "%");}' <(grep 'cpu ' /proc/stat) <(sleep 0.5; grep 'cpu ' /proc/stat))
 
-		# Ram Used
+		# Used Ram
 		RAM=$(awk '/^Mem/{print $3}' <(free -m)) 
 		
 		# Date and Time
